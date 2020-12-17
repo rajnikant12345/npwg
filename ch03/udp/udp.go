17d9ae809de902406f92ef0bd449db59caa06742
rajnikant <rajni@ves.io>
2020-12-17 17:46:51 +0530

    adding theory

--- ch03/udp/udp.go
+++ ch03/udp/udp.go
@@ -10,24 +10,24 @@ import (
 )
 
 func main() {
-	if len( os.Args) < 2 {
+	if len(os.Args) < 2 {
 		log.Fatalf("Please enter the sender address")
 	}
 	serverAddr := os.Args[1]
 	clientAddr := os.Args[2]
 
-	sa,err := net.ResolveUDPAddr("udp",serverAddr)
+	sa, err := net.ResolveUDPAddr("udp", serverAddr)
 	if err != nil {
 		log.Fatalf(" Listen closed with error: %v", err)
 	}
 
-	l, err := net.ListenUDP("udp",sa)
+	l, err := net.ListenUDP("udp", sa)
 
 	if err != nil {
 		log.Fatalf(" Listen closed with error: %v", err)
 	}
 
-	ca, err := net.ResolveUDPAddr("udp",clientAddr)
+	ca, err := net.ResolveUDPAddr("udp", clientAddr)
 	if err != nil {
 		log.Fatalf(" Listen closed with error: %v", err)
 	}
@@ -43,7 +43,7 @@ func main() {
 				fmt.Println("Exiting client loop ...")
 				break
 			}
-			_,err := l.WriteToUDP( []byte(text), ca )
+			_, err := l.WriteToUDP([]byte(text), ca)
 			if err != nil {
 				log.Printf(" Listen closed with error: %v\n", err)
 				break
@@ -52,18 +52,18 @@ func main() {
 		}
 	}()
 
-	message := make([]byte,1024)
+	message := make([]byte, 1024)
 
 	for {
-		n,add,err := l.ReadFrom(message)
+		n, add, err := l.ReadFrom(message)
 		if err != nil {
 			log.Printf(" Receiver closed with error: %v\n", err)
 			break
 		}
 		fmt.Println("======================================================")
-		fmt.Printf("Bytes Received: %d \n",n)
-		fmt.Printf("Address of sender: %s \n",add.String())
-		fmt.Printf("Message: %s",string(message[:n]))
+		fmt.Printf("Bytes Received: %d \n", n)
+		fmt.Printf("Address of sender: %s \n", add.String())
+		fmt.Printf("Message: %s", string(message[:n]))
 		fmt.Println("======================================================")
 
 	}
---
+++ ch04/TCP.md
@@ -0,0 +1,23 @@
+## HUGE TCP Theory
+
+
+### TCP ( Transmission Control Protocol )
+
+- TCP is called a connection-oriented, reliable transport protocol.
+- Guarantees data delivery, but slower then UDP.
+
+#### Message v/s Streams
+- Messages are One shot
+- Streams are like continuous flow
+
+
+#### TCP Offerings
+
+- Process-to-Process Communication
+- Stream Delivery Service
+- Full Duplex
+- TCP Connections
+- Reliability
+
+
+
